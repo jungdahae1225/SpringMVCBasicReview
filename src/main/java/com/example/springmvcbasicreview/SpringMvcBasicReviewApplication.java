15c9bc6a89cc564a5c8482ab469d812d032df93e
jungdahae1225 <jungdahae1225@gmail.com>
2022-03-25 02:02:58 +0900

    add view

--- src/main/java/com/example/springmvcbasicreview/SpringMvcBasicReviewApplication.java
+++ src/main/java/com/example/springmvcbasicreview/SpringMvcBasicReviewApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMvcBasicReviewApplication {
+public class  SpringMvcBasicReviewApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcBasicReviewApplication.class, args);
