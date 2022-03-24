8a48d312eed976161742f8a297c2f71bf2735032
jungdahae1225 <jungdahae1225@gmail.com>
2022-03-24 13:56:51 +0900

    Initial setting

---
+++ src/main/java/com/example/springmvcbasicreview/SpringMvcBasicReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcbasicreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBasicReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcBasicReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvcbasicreview/SpringMvcBasicReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcbasicreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBasicReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
